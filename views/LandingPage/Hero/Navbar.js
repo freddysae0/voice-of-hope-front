613e5f6f60023a8f5082b77d441a25dec667dee0
freddysae0 <freddysae00@gmail.com>
2024-09-16 12:50:12 +0200

    Adding Logo

--- views/LandingPage/Hero/Navbar.js
+++ views/LandingPage/Hero/Navbar.js
@@ -1,10 +1,13 @@
 // Supports weights 100-900
 import Button from "@/components/ui/Button";
 import "@fontsource-variable/roboto-serif";
+import Image from "next/image";
+import LogoImg from "@/assets/LandingPage/logo.png";
 const classLi = "flex justify-center items-center";
 export default function Hero() {
   return (
-    <nav className="absolute right-2 min-h-16 flex items-center justify-end pr-8">
+    <nav className="absolute right-2 min-h-16 flex items-center justify-between px-8 w-full">
+      <Image src={LogoImg} height={60}></Image>
       <ul
         className="flex flex-row justify-center gap-10 font-extrabold text-xl text-primary"
         style={{ fontFamily: "'Roboto Serif Variable', serif" }}
