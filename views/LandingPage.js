748f854b1694ef367b334428a2ea8272af4af7b2
freddysae0 <freddysae00@gmail.com>
2024-09-16 12:56:45 +0200

    Fast changes before Kamram Class

--- views/LandingPage.js
+++ views/LandingPage.js
@@ -2,10 +2,14 @@ import Hero from "@/views/LandingPage/Hero";
 import Mission from "@/views/LandingPage/Mission";
 import Near from "@/views/LandingPage/Near";
 import Sponsored from "@/views/LandingPage/Sponsored";
+import Head from "next/head";
 
 export default function Landing() {
   return (
     <div>
+      <Head>
+        <title>Voice of hope</title>
+      </Head>
       <Hero></Hero>
       <Mission></Mission>
       <Near></Near>
